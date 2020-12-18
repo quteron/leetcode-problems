ab09cdaeeaefd117be75f93d16efe898d5991c90
quteron <quteron@gmail.com>
2020-12-18 10:39:38 +0300

    Added soltion for problem 1-two-sum

---
+++ swift/easy/1-two-sum.playground/Contents.swift
@@ -0,0 +1,25 @@
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    // declare map to collect pairs as number -> its index in the array
+    var map = [Int: Int]()
+    // iterate over numbers in the array
+    for i in 0..<nums.count {
+        // get current number
+        let num = nums[i]
+        // calculate second number to have sum of target
+        let diff = target - num
+        // check whether has already passed second number
+        if let j = map[diff] {
+            // if yes, return indices
+            return [j, i]
+        } else {
+            // if no, save current number and its index
+            map[num] = i
+        }
+    }
+    
+    // should never be here
+    return [-1, -1]
+}
+
+let ans = twoSum([2,7,11,15], 9)
+print(ans)
